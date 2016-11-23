7fe6f6f36e40a43ba8b014494cc773f5c7ada198
m4jr0 <lowgravityisfun@gmail.com>
2016-11-23 17:13:45 -0500

    Implemented a pause menu and a main menu.

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+    public string RootScene;
+    public GameObject PauseMenu;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+        if (this.PauseMenu != null && Input.GetButtonDown("Pause")) {
+            this.LaunchPause();
+    
+        }
+    }
+
+    public void LaunchRootScene() {
+        SceneManager.LoadScene(this.RootScene);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void LaunchPause() {
+        if (this.PauseMenu == null) return;
+
+        bool isPause = this.PauseMenu.activeSelf;
+
+        if (isPause) {
+            Time.timeScale = 1f;
+        } else {
+            Time.timeScale = 0f;
+        }
+
+        this.PauseMenu.SetActive(!isPause);
+    }
+}
