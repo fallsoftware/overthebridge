0a1c4d3a5d9448904dad2ad5c2c8c719f903476b
m4jr0 <lowgravityisfun@gmail.com>
2016-11-25 21:54:22 -0500

    Started small moving platforms.

--- Assets/Scripts/Items/ShadowLightPlatform/ShadowLightPlatform.cs
+++ Assets/Scripts/Items/ShadowLightPlatform/ShadowLightPlatform.cs
@@ -7,6 +7,7 @@ public class ShadowLightPlatform : MonoBehaviour {
     public float AerialTime=0.1f;
     public float acceleration=0.01f;
     public float speed = 0;
+    public bool grounded = false;
     public float maxspeed = 2;
     private float rayspeed = 0;
     private Vector2 InitialPosition;
@@ -15,22 +16,34 @@ public class ShadowLightPlatform : MonoBehaviour {
 	void Start () {
         InitialPosition = this.transform.position;
 	}
-	public void ApllyForce(Vector2 Velocity)
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Ground")) {
+            grounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider) {
+        if (collider.CompareTag("Ground")) {
+            grounded = false;
+        }
+    }
+
+    public void ApllyForce(Vector2 Velocity)
     {
-        if (rayspeed < maxspeed)
-        {
-            rayspeed += acceleration;
+        if (grounded == false) {
+            if (rayspeed < maxspeed) {
+                rayspeed += acceleration;
+            }
+            gameObject.GetComponent<Rigidbody2D>().velocity = Velocity.normalized * rayspeed;
+            if (gravity) {
+                startTime = Time.time;
+            }
         }
-        gameObject.GetComponent<Rigidbody2D>().velocity = Velocity.normalized*rayspeed;
-        if (gravity)
-        {
-            startTime = Time.time;
-        }
+
     }
     // Update is called once per frame
     void Update() {
-        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        if (gravity && startTime != 0) { 
+        if (gravity && !grounded) { 
             float timeleft = Time.time - startTime;
             if(AerialTime - timeleft<=0)
             {
@@ -40,14 +53,7 @@ public class ShadowLightPlatform : MonoBehaviour {
                     speed += acceleration;
                 }
                 Vector2 CurrentPosition = this.transform.position;
-                if ((InitialPosition - CurrentPosition).magnitude < speed)
-                {
-                    gameObject.GetComponent<Rigidbody2D>().MovePosition(InitialPosition);
-                }
-                else
-                {
-                    gameObject.GetComponent<Rigidbody2D>().MovePosition(CurrentPosition + (InitialPosition - CurrentPosition).normalized * speed);
-                }
+                gameObject.GetComponent<Rigidbody2D>().MovePosition(CurrentPosition + Vector2.down * speed);
                 
 
             }
