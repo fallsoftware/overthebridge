d5ca9a026123f10f05d971dbc19e71c16ec3f54a
m4jr0 <lowgravityisfun@gmail.com>
2016-12-03 02:02:28 -0500

    Improved sound management.

--- Assets/Scripts/Sounds/SoundManager.cs
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -4,15 +4,39 @@ using System.Collections.Generic;
 
 public class SoundManager : MonoBehaviour {
     public Dictionary<string, AudioSource> FxSources;
-    public AudioSource MusicSource;
+    [HideInInspector]
+    public GameObject MusicObject;
     public static SoundManager Instance = null;
-    [Range(0f, 1f)] public float MusicVolume = 1f;
-    [Range(0f, 1f)] public float FxVolume = 1f;
+
+    [SerializeField]
+    public float MusicVolume {
+        get { return this.musicVolume; }
+        set {
+            this.musicVolume = value;
+            this.UpdateMusicVolume();
+        }
+    }
+
+    [Range(0f, 1f)]
+    public float musicVolume = 1f;
+
+    [SerializeField]
+    public float FxVolume {
+        get { return this.fxVolume; }
+        set {
+            this.fxVolume = value;
+            this.UpdateFxVolume();
+        }
+    }
+
+    [Range(0f, 1f)]
+    public float fxVolume = 1f;
 
     public float LowPitchRange = .95f;
     public float HighPitchRange = 1.05f;
 
-    void Start() {}
+    void Start() {
+    }
 
     void Update() {}
 
@@ -64,9 +88,10 @@ public class SoundManager : MonoBehaviour {
         fxSource.Play();
     }
 
-    public void PlayMusic(AudioSource musicSource) {
-        musicSource.Play();
-        StartCoroutine(this.fadeOut(musicSource));
+    public void PlayMusic(GameObject musicObject) {
+        Music music = musicObject.GetComponent<Music>();
+        music.MusicSource.Play();
+        StartCoroutine(this.fadeOut(musicObject));
     }
 
     public void SetMusicVolume(float musicVolume) {
@@ -81,24 +106,40 @@ public class SoundManager : MonoBehaviour {
         this.FxVolume = fxVolume;
     }
 
-    public IEnumerator fadeOut( AudioSource newMusic) {
+    public IEnumerator fadeOut(GameObject newMusicObject) {
         float duration = 2f;
         float startTime = Time.time;
         float endTime = startTime + duration;
+        Music newMusic = newMusicObject.GetComponent<Music>();
 
 
         while (Time.time < endTime) {
             float i = (Time.time - startTime) / duration;
 
-            if (this.MusicSource != null) {
-                this.MusicSource.volume = (1 - i) * this.MusicVolume;
+            if (this.MusicObject != null) {
+                Music oldMusic = this.MusicObject.GetComponent<Music>();
+                oldMusic.MusicSource.volume = (1 - i) * this.MusicVolume;
             }
 
-            newMusic.volume = i * this.MusicVolume;
+            newMusic.MusicSource.volume = i * this.MusicVolume;
 
             yield return null;
         }
 
-        this.MusicSource = newMusic;
+        Destroy(this.MusicObject);
+        this.MusicObject = newMusicObject;
+        DontDestroyOnLoad(this.MusicObject);
+    }
+
+    public void UpdateFxVolume() {
+        foreach (KeyValuePair<string, AudioSource> fxSource
+            in this.FxSources) {
+            fxSource.Value.volume = this.FxVolume;
+        }
+    }
+
+    public void UpdateMusicVolume() {
+        Music music = this.MusicObject.GetComponent<Music>();
+        music.MusicSource.volume = music.Volume * this.MusicVolume;
     }
 }
