874574e5ab32da574261c12d739381ca787739a9
m4jr0 <lowgravityisfun@gmail.com>
2016-12-04 01:21:19 -0500

    Added rain.

---
+++ Assets/Scripts/Camera/SetCameraToMakeItRain.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DigitalRuby.RainMaker;
+
+public class SetCameraToMakeItRain : MonoBehaviour {
+    public RainScript2D RainScript2D;
+
+	void Awake() {
+	    this.RainScript2D.Camera = Camera.main;
+	}
+
+    void Start() {
+        
+    }
+
+	void Update() {
+	
+	}
+}
