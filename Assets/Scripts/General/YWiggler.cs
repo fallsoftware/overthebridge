f77c6728834692a12be7e5d57f18b4902b756b77
m4jr0 <lowgravityisfun@gmail.com>
2016-12-10 18:19:31 -0500

    Added ending.

---
+++ Assets/Scripts/General/YWiggler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YWiggler : MonoBehaviour {
+    private float YDelta = 3f;
+    private float YSpeed = 2f;
+
+    private Vector3 _position;
+    private float _minY;
+    private float _maxY;
+    private int _direction = 1;
+
+    void Start () {
+        this._position = this.transform.position;
+        this._minY = this._position.y - this.YDelta;
+        this._maxY = this._position.y + this.YDelta;
+    }
+
+	void Update () {
+	    this.changePosition();
+	}
+
+    private void changePosition() {
+        if (this._position.y - this._minY < 0.2f 
+            || this._position.y - this._maxY > 0.2f) {
+            this._direction = -this._direction;
+        }
+
+        float newY
+            = this._direction * this.YSpeed * Time.deltaTime 
+                + this._position.y;
+
+        this._position = new Vector3(this._position.x, newY, this._position.z);
+        this.transform.position = this._position;
+    }
+}
---
+++ Assets/Scripts/Items/Crystal/CrystalState.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalState : MonoBehaviour {
+    public float ShineSpeed = 0.8f;
+    public float ShinePeriod = 3f;
+    public AudioClip PortalSound;
+
+    private AudioSource _audioSource;
+    private Renderer _renderer;
+    private float _defaultShineLocation;
+    private bool _wait = false;
+
+    void Start () {
+        this._renderer = this.GetComponent<Renderer>();
+        this._defaultShineLocation
+            = this._renderer.material.GetFloat("_ShineLocation");
+        this._renderer.material.SetFloat("_ShineWidth", this.computeShineWidth(
+            this._defaultShineLocation));
+    }
+
+	void Update () {
+	    if (this._wait) return;
+
+        float oldShineLocation
+            = this._renderer.material.GetFloat("_ShineLocation");
+        this.setShininess(oldShineLocation);
+
+	    if (this._renderer.material.GetFloat("_ShineWidth") <= 0.05f) {
+	        StartCoroutine(this.HandleIt());
+	    }
+    }
+
+    private IEnumerator HandleIt() {
+        this._wait = true;
+
+        yield return new WaitForSeconds(this.ShinePeriod);
+
+        this._wait = false;
+    }
+
+    private void setShininess(float oldShineLocation) {
+        float newShineLocation
+            = oldShineLocation + (this.ShineSpeed * Time.deltaTime);
+        newShineLocation %= 1;
+        this._renderer.material.SetFloat("_ShineLocation", newShineLocation);
+        this._renderer.material.SetFloat(
+            "_ShineWidth", this.computeShineWidth(newShineLocation));
+    }
+
+    private float computeShineWidth(float shineLocation) {
+        float shineWidth = 2 * shineLocation;
+        const float middle = 0.5f;
+
+        if (shineWidth > 2 * middle) {
+            shineWidth = this.computeShineWidth(2 * middle - shineLocation);
+        }
+
+        return shineWidth;
+    }
+}
